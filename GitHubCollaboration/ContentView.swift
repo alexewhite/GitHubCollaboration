d4c9ea2e7833e059d3f38fb6e9570c5172f8aa28
Charlotte <caw680@gmail.com>
2025-08-11 10:08:26 -0400

    Added 1 change to ContentView

--- GitHubCollaboration/ContentView.swift
+++ GitHubCollaboration/ContentView.swift
@@ -14,7 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-            Text("Change 3")
+            Text("Change 4")
+            
         }
         .padding()
     }
